8530a99d50864f172c94d21885fbb110f8ac2148
SahilM09 <168789699+SahilM09@users.noreply.github.com>
2024-05-12 23:01:19 +0000

    basics_datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict" // treat js code as per newer version
+
+let name = "Sahil"
+let age = 23
+let isLoggedIn = true
+let state;
+
+/*number => 2to the power 53
+string
+boolean
+null=> standalone value
+undefined
+*/
+
+console.log(typeof  age)
