2011b9286b71a8a1ee7a7d53426ad5938678d321
SahilM09 <168789699+SahilM09@users.noreply.github.com>
2024-05-13 10:40:37 +0000

    conversion of datatypes

---
+++ basics/dtconversion.js
@@ -0,0 +1,41 @@
+let score = 22
+let age = "22ab"
+console.log(typeof score);
+console.log(typeof (score));
+console.log(typeof age);
+
+
+let valueInNumber = Number(age)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "22" => 22
+//"22abc" => NaN
+//true=> 1 false=>0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let isLogged = ""
+let  booleanIsLogged = Boolean (isLogged)
+console.log(booleanIsLogged);
+
+let boolString ="sahil"
+let booleanboolString = Boolean(boolString)
+console.log(booleanboolString);
+
+
+// 1 =>true
+//"" => false
+//"sahil" => true
+
+
+let SomeNumber = 23
+let stringNumber = String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
