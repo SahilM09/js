6e9fdf44ab3fc7fe38d54f847647c684a518a8ce
SahilM09 <168789699+SahilM09@users.noreply.github.com>
2024-05-14 23:24:50 +0000

    basics of math function also num

---
+++ basics/num_math.js
@@ -0,0 +1,38 @@
+const score = 100
+
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+// console.log(balance.toString().length); 
+// console.log(balance.toFixed('2'));
+
+// const newNo = new Number(575.786887)
+
+// console.log(newNo.toPrecision(7));
+
+// const otherNo = new Number(10000)
+
+// console.log(otherNo.toLocaleString('en-in'));
+
+//********MATHS*******
+
+console.log(Math);
+
+
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(5.67));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.1));
+// console.log(Math.pow(2,4));
+// console.log(Math.min(2,4,5,7));
+// console.log(Math.max(2,4,5,7));
+
+console.log((Math.random()*10) +1);
+console.log((Math.random()*10) +1);
+
+const min =1
+const max =6
+console.log(Math.floor(Math.random()* (max - min + 1)+min))
